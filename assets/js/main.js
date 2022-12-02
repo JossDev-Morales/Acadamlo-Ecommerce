810245e4060a6d9a404375537d24d95a3f45f76d
Josue Morales <thejosuescript@gmail.com>
2022-12-02 13:42:39 -0600

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'assets/images/featured1.png',
+      category: 'hoodies',
+      quantity: 10
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'assets/images/featured2.png',
+      category: 'shirts',
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'assets/images/featured3.png',
+      category: 'shirts',
+      quantity: 20
+    }
+  ]
